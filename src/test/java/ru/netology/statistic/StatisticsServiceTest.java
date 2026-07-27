54b1adda4d5b2eea3f70b09059eb16aa2cf985fc
Anna Tropina <anna.tropina.1026@yandex.ru>
2026-07-27 17:04:55 +0300

    coverage

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -16,6 +16,36 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxWhenFirstIsMax() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {100, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 100;
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWhenAllEqual() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {5, 5, 5, 5, 5};
+        long expected = 5;
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void findMaxWhenMaxInMiddle() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {5, 8, 12, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+    }
+
+
 }
 
 
